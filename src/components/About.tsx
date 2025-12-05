3b944fe69728b6164dc43de986d98996a114a870
Achraf Reyani <achrafreyani99@gmail.com>
2025-12-05 10:56:24 +0900

    auto lint about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,7 +23,6 @@ export function About() {
           </div>
         </div>
 
-
         {/* Text content */}
         <div className="md:w-2/3 mt-8 md:mt-0 md:pl-12 text-center md:text-left max-w-3xl">
           <h2 className="text-4xl font-bold mb-6">{t('title')}🙋</h2>
