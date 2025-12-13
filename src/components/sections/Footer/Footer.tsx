817896cdd9cfce7226d31ec8ade45543341474b9
Achraf Reyani <achrafreyani99@gmail.com>
2025-12-13 23:49:38 +0900

    move footer component to new refactored section

---
+++ src/components/sections/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { FooterScrollTop } from "./FooterScrollTop";
+
+export function Footer() {
+  return (
+    <footer className="bg-bg-light dark:bg-bg-dark w-full relative h-16 flex justify-center items-center">
+      <FooterScrollTop />
+    </footer>
+  );
+}
