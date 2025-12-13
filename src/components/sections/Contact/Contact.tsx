3062e4fee7db87f8945c830c2d51fbc019d9bc6c
Achraf Reyani <achrafreyani99@gmail.com>
2025-12-13 23:47:18 +0900

    refactor contact section

---
+++ src/components/sections/Contact/Contact.tsx
@@ -0,0 +1,10 @@
+import {Section} from '@/components/sections/About/Section';
+import {ContactContainer} from './ContactContainer';
+
+export function Contact() {
+  return (
+    <Section id="contact">
+      <ContactContainer />
+    </Section>
+  );
+}
