c1bc6a39770ab1abd01414fc95a64adeaf91357e
Achraf Reyani <achrafreyani99@gmail.com>
2026-01-11 16:37:31 +0900

    remove basic layout elements from page.tsx to move into layout.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,19 +1,15 @@
-import Header from '@/components/sections/Header/Header';
-import { Home } from '@/components/sections/Home/Home';
-import { About } from '@/components/sections/About/About';
-import { Portfolio } from '@/components/sections/Portfolio/Portfolio';
-import { Contact } from '@/components/sections/Contact/Contact';
-import { Footer } from '@/components/sections/Footer/Footer';
+import {Home} from '@/components/sections/Home/Home';
+import {About} from '@/components/sections/About/About';
+import {Portfolio} from '@/components/sections/Portfolio/Portfolio';
+import {Contact} from '@/components/sections/Contact/Contact';
 
 export default function IndexPage() {
   return (
     <>
-      <Header />
       <Home />
       <About />
       <Portfolio />
       <Contact />
-      <Footer />
     </>
   );
 }
