14c295b282f9b8720f95ca5e278afba44094c56e
Achraf Reyani <achrafreyani99@gmail.com>
2026-01-11 16:05:02 +0900

    remove unnecessary code for root layout page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,16 +5,7 @@ import { Portfolio } from '@/components/sections/Portfolio/Portfolio';
 import { Contact } from '@/components/sections/Contact/Contact';
 import { Footer } from '@/components/sections/Footer/Footer';
 
-import {Locale} from 'next-intl';
-import {use} from 'react';
-
-type Props = {
-  params: Promise<{locale: Locale}>;
-};
-
-export default function IndexPage({params}: Props) {
-  const {locale} = use(params);
-
+export default function IndexPage() {
   return (
     <>
       <Header />
