79371a44ead6e19b8dc3cc08b016128b67f58e4a
Achraf Reyani <achrafreyani99@gmail.com>
2026-01-21 20:58:33 +0900

    add arabic

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,6 @@
 import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
-  locales: ['en', 'ja', 'nl'],
+  locales: ['en', 'ja', 'nl', 'ar'],
   defaultLocale: 'en'
 });
